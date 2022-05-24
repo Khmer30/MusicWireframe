21f2081f795a7d4e8a5bf15ec001a0265392d37a
marie <dul.sreyneang@gmail.com>
2022-05-24 09:09:44 +0800

    Make circular finger shadow -reverseBackground -playPauseBackground -forwardBackground

--- Wireframe/ViewController.swift
+++ Wireframe/ViewController.swift
@@ -17,13 +17,17 @@ class ViewController: UIViewController {
     @IBOutlet var playPauseButton: UIButton!
     @IBOutlet var forwardButton: UIButton!
     
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
        
+        reverseBackground.layer.cornerRadius = reverseBackground.frame.height / 2
+        reverseBackground.clipsToBounds = true
+        playPauseBackground.layer.cornerRadius = playPauseBackground.frame.height / 2
+        playPauseBackground.clipsToBounds = true
+        forwardBackground.layer.cornerRadius = forwardBackground.frame.height / 2
+        forwardBackground.clipsToBounds = true
     }
-
-
 }
 
