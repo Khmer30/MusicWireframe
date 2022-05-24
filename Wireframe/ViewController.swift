0ff705731605713431e815149e885f93db8c4abc
marie <dul.sreyneang@gmail.com>
2022-05-24 09:14:02 +0800

    Update the play/pause button's selection state -use property observers to automatically

--- Wireframe/ViewController.swift
+++ Wireframe/ViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var isPlaying: Bool = true {
+        didSet {
+            playPauseButton.isSelected = isPlaying
+        }
+    }
 
     @IBOutlet var albumImageView: UIImageView!
     @IBOutlet var reverseBackground: UIView!
