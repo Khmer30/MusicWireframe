75b488742a3d353683594a901629655d1ceaa999
marie <dul.sreyneang@gmail.com>
2022-05-24 09:00:05 +0800

    Add @IBOutlet

--- Wireframe/ViewController.swift
+++ Wireframe/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var albumImageView: UIImageView!
+    @IBOutlet var reverseBackground: UIView!
+    @IBOutlet var playPauseBackground: UIView!
+    @IBOutlet var forwardBackground: UIView!
+    @IBOutlet var reverseButton: UIButton!
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var forwardButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
